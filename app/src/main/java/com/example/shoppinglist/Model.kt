f9188a8b4de52d9f421555eaa92950e03776ab48
avidaldo <avd.formacion@gmail.com>
2023-03-07 16:57:04 +0100

    función estilo Java para comparar con estilo Kotlin

--- app/src/main/java/com/example/shoppinglist/Model.kt
+++ app/src/main/java/com/example/shoppinglist/Model.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.setValue
 class ShoppingProduct(val key: Int, val name: String, initialChecked: Boolean = false) {
     var checked by mutableStateOf(initialChecked)
 }
+
 /**
  * Ahora la clase ShoppingProduct mantiene la definición de key y name como propiedades inmutables
  * en su constructor primario, pero checked pasa a ser una propiedad mutable que podrá ser
@@ -16,6 +17,13 @@ class ShoppingProduct(val key: Int, val name: String, initialChecked: Boolean = 
  */
 
 
-
 fun getFakeShoppingProducts() = List(30) { i -> ShoppingProduct(i, "Producto $i") }
+
+/* La misma función estilo Java: */
+/*
+fun getFakeShoppingProducts(): List<ShoppingProduct> {
+    var lista = List(30) { i -> ShoppingProduct(i, "Producto $i") }
+    return lista
+}
+*/
 
