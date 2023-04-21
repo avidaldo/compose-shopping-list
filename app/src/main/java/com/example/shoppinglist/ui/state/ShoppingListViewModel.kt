8efc8e5a9170562baca5a996407fe84869189368
Alejandro Vidal <avd.formacion@gmail.com>
2023-04-21 21:03:33 +0200

    añadida explicacion

--- app/src/main/java/com/example/shoppinglist/ui/state/ShoppingListViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/ui/state/ShoppingListViewModel.kt
@@ -7,10 +7,10 @@ import kotlinx.coroutines.flow.update
 
 class ShoppingListViewModel : ViewModel() {
 
-    private val _list = MutableStateFlow(setOf<String>())
+    private val _list = MutableStateFlow(setOf<String>())  // (1)
     val list = _list.asStateFlow()
 
-    fun add(item: String) : Boolean {
+    fun add(item: String) : Boolean { // (1)
         var result = false
         _list.update {
             it.toMutableSet().apply {
@@ -26,6 +26,14 @@ class ShoppingListViewModel : ViewModel() {
         }
     }
 
-
-
 }
+
+/**
+ * Los conjuntos (Set) no permiten elementos duplicados, por lo que en esta versión (ojo que
+ * también he simplificado cada elemento a un mero String con el nombre del producto, ya no hay
+ * checkBox) no será necesario comprobar manualmente si hay duplicados antes de añadir elementos.
+ *
+ * El propio método add del Set devolverá false cuando el nuevo elemento a añadir ya exista previamente
+ * en el conjunto.
+ *
+ */
